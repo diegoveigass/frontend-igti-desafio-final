bf79dc7e789de9159b5d1f5b8e3fe110193ea3b3
Diego Veiga Santos <sdiegoveiga@gmail.com>
2020-09-06 09:54:54 -0300

    config env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://desafio-final-igti-backend.herokuapp.com/api/transaction',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
